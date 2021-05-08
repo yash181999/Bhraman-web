f03e3b247b9d8ae1ce1a6cde9d49b4b0a1887cbe
yashmehta <yash181999@gmail.com>
2021-05-08 22:49:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.png';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar/>
+    </div>
+  );
+}
+
+export default App;
